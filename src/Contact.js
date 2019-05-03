e1d91aede00ac0869c7e9df51a1c58fa659b0ab0
Dion Roloff <dion.roloff@gmail.com>
2019-05-03 12:49:13 -0500

    initial commit

---
+++ src/Contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+          <div align="left">
+            <h3>Contact Details</h3>
+            <p>(651)208-3361</p>
+            <p>dion.roloff@gmail.com</p>
+            <p>
+                <a target="_blank" rel="noopener noreferrer" href="http://dionroloff.codes">My Website</a>
+            </p>
+          </div>
+        );
+    }
+}
+
+export default Contact
---
+++ src/Profile.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+    render() {
+        return(
+            <div>
+                <h3 align="left">Professional Profile</h3>
+                <p align="left">
+                    - Former English-as-a-second-language instructor who taught himself to code between classes<br/>
+                    - A passion for problem-solving and coding for good<br/>
+                    - Insatiably curious
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Profile
